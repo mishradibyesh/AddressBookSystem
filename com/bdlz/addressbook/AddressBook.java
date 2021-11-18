a35ef00d6e1b705349402f05127f84f7fa5eea76
mishradibyesh <dibyeshmishra207@gmail.com>
2021-11-18 19:58:37 +0530

    ADDED: code to add more than one addressbook

--- com/bdlz/addressbook/AddressBook.java
+++ com/bdlz/addressbook/AddressBook.java
@@ -1,28 +1,53 @@
 package com.bdlz.addressbook;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Scanner;
 
 public class AddressBook {
 	/*
-   Declaring The Add Contact Method
-   And Entering The Contact Details By Using Scanner Class
-   And Printing The Contact Details Of Person
+	 *  creating multiple addressbook with the help of HashMap
+	 * 
 	 */
 
-	List<ContactDetails> contactDetailsList = new ArrayList<>();
+	HashMap <String , ArrayList<ContactDetails>> hashmap = new HashMap();
 	Scanner scanner = new Scanner(System.in);
-	public void addContact() {
-		System.out.println("Enter the number of contacts you want to enter");
-		int number = scanner.nextInt();
-		for (int i = 0; i < number; i++) {
-			System.out.println("Enter the contact details of person ");
-			writeContact();
+
+	public void addAddressBook() {
+		System.out.println("Enter the name of the addressBook");
+		String bookName = scanner.nextLine();
+
+		if(hashmap.containsKey(bookName)) {
+			System.out.println("This BookName is already entered ! please type another name");
+			addAddressBook();
+		}
+		else {
+			List<ContactDetails> contactDetailsList = new ArrayList<ContactDetails>();
+			hashmap.put(bookName, (ArrayList<ContactDetails>) contactDetailsList);
+			System.out.println("AddressBook Added successfully!");
+
+		}
+		System.out.println("do you want to add more addressbook ? press 1 if yes");
+		int choice = scanner.nextInt();
+		if(choice == 1) {
+			addAddressBook();
 		}
 	}
-	public void writeContact() {
-		ContactDetails person = new ContactDetails();
+	public void addContact() {
+		System.out.println("Enter the name of Address book to add the contact.");
+		String newContact = scanner.nextLine();
+		if(hashmap.containsKey(newContact)) {
+			writeContact(hashmap.get(newContact));
+		}
+		else {
+			System.out.println("this addressBook is not exist");
+		}
+
+	}
+	// method to write the contact details
+	public void writeContact(ArrayList list) {
+		ContactDetails contact = new ContactDetails();
 		System.out.println("Enter First Name : ");
 		String firstName = scanner.next();
 		System.out.println("Enter Last Name : ");
@@ -39,9 +64,9 @@ public class AddressBook {
 		long mobileNumber = scanner.nextLong();
 		System.out.println("Enter EmailId : ");
 		String emailId = scanner.next();
-		person = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
-		contactDetailsList.add(person);
-		printContact();
+		contact = new ContactDetails(firstName, lastName, address, city, state, zipCode, mobileNumber, emailId);
+		list.add(contact);
+
 	}
 
 	/*
@@ -52,20 +77,30 @@ public class AddressBook {
 	 */
 
 	public void editContact() {
-		System.out.println("Enter the first name of person to edit contact");
-		String editName = scanner.next();
-		boolean edited = false;
-		for (int i = 0; i < contactDetailsList.size(); i++) {
-			String name = contactDetailsList.get(i).getFirstName();
-			if (name.equalsIgnoreCase(editName)) {
-				writeContact();
-				edited = true;
-				break;
+		System.out.println("Enter the name of Address book to edit the contact.");
+		scanner.nextLine();
+		String newContact = scanner.nextLine();
+		if(hashmap.containsKey(newContact)) {
+			System.out.println("Enter the first name of person to edit contact");
+			String editName = scanner.next();
+			boolean edited = false;
+			for (int i = 0; i < hashmap.get(newContact).size(); i++) {
+				String name = hashmap.get(newContact).get(i).getFirstName();
+				if (name.equalsIgnoreCase(editName)) {
+					writeContact(hashmap.get(newContact));
+					edited = true;
+					break;
+				}
 			}
+			if (!edited) {
+				System.out.println("enter name is incorrect");
+			}	
+
 		}
-		if (!edited) {
-			System.out.println("enter name is incorrect");
+		else {
+			System.out.println("the entered addressbook is not in existence");
 		}
+
 	}
 
 	/*
@@ -76,35 +111,50 @@ public class AddressBook {
 	 */
 
 	public void deleteContact() {
-		System.out.println("Enter the first name of person to delete contact");
-		String deleteName = scanner.next();
-		int i = 0;
-		for ( ;i < contactDetailsList.size(); i++) {
-			String name = contactDetailsList.get(i).getFirstName();
-			if (name.equalsIgnoreCase(deleteName)) {
-				break;
+		System.out.println("Enter the name of Address book to delete the contact.");
+		scanner.nextLine();
+		String newContact = scanner.nextLine();
+		if(hashmap.containsKey(newContact)) {
+			System.out.println("Enter the first name of person to delete contact");
+			String deleteName = scanner.next();
+			int i = 0;
+			for ( ;i < hashmap.get(newContact).size(); i++) {
+				String name = hashmap.get(newContact).get(i).getFirstName();
+				if (name.equalsIgnoreCase(deleteName)) {
+					break;
+				}
+			}
+			if (i < hashmap.get(newContact).size()) {
+				hashmap.get(newContact).remove(i);
+				System.out.println("Contact Deleted");			}
+			else {
+				System.out.println("Contact not found");
 			}
 		}
-		if (i < contactDetailsList.size()) {
-			contactDetailsList.remove(i);
-			System.out.println("Contact Deleted");
-			System.out.println("Remaining contacts in the book isgit");
-			printContact();
-		}else {
-			System.out.println("Contact not find");
+		else {
+			System.out.println("the entered addressbook is not in existence");
 		}
 
 	}
 	public void printContact() {
-		for (int i = 0; i < contactDetailsList.size(); i++) {
-			System.out.println("Contact Details");
-			System.out.println("Name         : " + contactDetailsList.get(i).getFirstName()+ " " + contactDetailsList.get(i).getLastName() + "\n"
-					+ "Address      : " + contactDetailsList.get(i).getAddress()   + "\n"
-					+ "City         : " + contactDetailsList.get(i).getCity()      + "\n"
-					+ "State        : " + contactDetailsList.get(i).getState()     + "\n"
-					+ "ZipCode      : " + contactDetailsList.get(i).getZipCode()   + "\n"
-					+ "MobileNumber : " + contactDetailsList.get(i).getMobileNo()  + "\n"
-					+ "EmailId      : " + contactDetailsList.get(i).getEmailId()   + "\n");
+		System.out.println("Enter the name of Address book to print the contact.");
+		scanner.nextLine();
+		String contact = scanner.nextLine();
+		if(hashmap.get(contact).isEmpty()) {
+			System.out.println("the entered addressbook is empty!");
 		}
+		if(hashmap.containsKey(contact)) {
+			for (int i = 0; i < hashmap.get(contact).size(); i++) {
+				System.out.println("Contact Details");
+				System.out.println("Name         : " + hashmap.get(contact).get(i).getFirstName()+ " " + hashmap.get(contact).get(i).getLastName() + "\n"
+						+ "Address      : " + hashmap.get(contact).get(i).getAddress()   + "\n"
+						+ "City         : " + hashmap.get(contact).get(i).getCity()      + "\n"
+						+ "State        : " + hashmap.get(contact).get(i).getState()     + "\n"
+						+ "ZipCode      : " + hashmap.get(contact).get(i).getZipCode()   + "\n"
+						+ "MobileNumber : " + hashmap.get(contact).get(i).getMobileNo()  + "\n"
+						+ "EmailId      : " + hashmap.get(contact).get(i).getEmailId()   + "\n");
+			}
+		}
+
 	}
 }
--- com/bdlz/addressbook/AddressBookMain.java
+++ com/bdlz/addressbook/AddressBookMain.java
@@ -7,9 +7,12 @@ public class AddressBookMain {
 		AddressBook addressBook = new AddressBook();
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
-			System.out.println("Enter \n 1 To add The contact \n 2 To edit the contact \n 3 To delete the contact  \n 4 To print the contact   \n 5 To exist");
+			System.out.println("Enter \n 0 To add The AddressBook \n 1 To add The contact \n 2 To edit the contact \n 3 To delete the contact  \n 4 To print the contact   \n 5 To exist");
 			int choice = scanner.nextInt();
 			switch (choice) {
+			case 0:
+				addressBook.addAddressBook();
+				break;
 			case 1:
 				addressBook.addContact();
 				break;
@@ -23,10 +26,11 @@ public class AddressBookMain {
 				addressBook.printContact();
 				break;
 			case 5:
+				System.out.println("Exit successfully!");
 				System.exit(0);
 				break;
 			default:
-				System.out.println("Enter the wrong input");
+				System.out.println(" you Entered the wrong input");
 				continue;
 			}
 
